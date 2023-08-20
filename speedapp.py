8878d8b938efdb27680554927d79d5d7ee968aba
Justin Blair <jbeta137@gmail.com>
2023-08-19 20:45:47 -0400

    fixed strava token exchange

--- speedapp.py
+++ speedapp.py
@@ -8,21 +8,24 @@ app = Flask(__name__)
 
 CLIENT_ID = os.environ.get('STRAVA_CLIENT_ID')
 CLIENT_SECRET = os.environ.get('STRAVA_CLIENT_SECRET')
-REDIRECT_URI = 'http://gyronautilus.com/speedapp/loggedin'
+REDIRECT_URI = 'http://www.gyronautilus.com/speedapp/loggedin'
 
 @app.route("/")
 def test():
 	testpost = {'heading': "testing title", 'body': 'testing body'}
-	return render_template('index.html', post=testpost)
+	return render_template('splash.html', post=testpost)
 
-@app.route("/loggedin", methods=['POST'])
+@app.route("/loggedin", methods=['GET'])
 def init():
 	activities = {}
 	name = 'No Name!'
-	if request.method == "POST":
-		athlete = request.args.get("athlete")
-		if athlete:
-			name = athlete
+	if request.method == "GET":
+		code = request.args.get('code')
+		if not code:
+			return "Missing code parameter!", 400		
+		strava_response = exchange_token(code).json()
+		name = strava_response['athlete']['firstname']
+		userid = strava_response['athlete']['id']
 	return render_template('loggedin.html',name=name,activities=activities)
 
 @app.route("/auth", methods=['GET'])
@@ -36,22 +39,17 @@ def authorize():
 		}
 	return redirect('{}?{}'.format(url, urllib.urlencode(params)))
 
-@app.route("/token", methods=['GET'])
-def return_token():
-	code = request.args.get('code')
-	if not code:
-		return "Missing code parameter!", 400
+def exchange_token(code):
 	strava_req = pyreq.post(
 		'https://www.strava.com/oauth/token',
 		data={
 			'client_id': CLIENT_ID,
-            'client_secret': CLIENT_SECRET,
-            'code': code,
-            'grant_type': 'authorization_code'
-        }
+            		'client_secret': CLIENT_SECRET,
+            		'code': code,
+            		'grant_type': 'authorization_code'
+        	}
 	)
-	return jsonify(strava_req)
-	
+	return strava_req
 
 @app.route("/webhook",methods=['POST', 'GET'])
 def webhook():
