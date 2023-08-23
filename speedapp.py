3f5ee2cfe354775fae054180c521dddc6f93acd5
Justin Blair <jbeta137@gmail.com>
2023-08-23 16:47:48 -0500

    fix import error

--- speedapp.py
+++ speedapp.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, redirect, url_for, session
 import urllib
-import datetime
+from datetime import datetime
 import requests as pyreq
 import init
 import db
